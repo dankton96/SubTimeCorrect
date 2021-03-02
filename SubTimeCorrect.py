b9e9d1531a4b48d4965996c6b45dcde5d4bb2a9a
dankton96 <dankton96@hotmail.com>
2021-03-02 22:49:26 +0000

    Implemented working version of delay adding to subtitle

--- SubTimeCorrect.py
+++ SubTimeCorrect.py
@@ -12,8 +12,6 @@ FilePath=easygui.fileopenbox("Selecione o arquivo a ser manipulado:")
 file=open(FilePath,'r+')
 sub=[]
 ################################################################################################
-#line example
-#00:00:04,084 --> 00:00:05,920
 NumPos=[0,1,3,4,6,7,9,10,11,17,18,20,21,23,24,26,27,28]
 CommaPos=[8,25]
 ColonPos=[2,5,19,22]
@@ -48,9 +46,6 @@ for l in lines:
         i+=1
     else:
         i+=1
-#for i in places2calc:
-#    print(lines[i])
-#time diference to correct -00:01:26,647
 
 instructions='''
 
@@ -60,13 +55,10 @@ Onde: H=Horas, M=Minutos, S=Segundos, s=milissegundos
 
 Insira o tempo a ser ajustado:
 '''
-#'10/10/2000 00:01:26,647'
 TimeDifStr='+'
 while(TimeDifStr[0]!='-' and TimeDifStr[0] not in numList):
     ClearScreen()
     TimeDifStr=input(instructions)
-#print(TimeDifStr)
-#input("pressione enter para encerrar")
 if(TimeDifStr[0]=='-'):
     IsForward=True
     TimeDifStr=TimeDifStr[1:]
@@ -74,8 +66,6 @@ else:
     IsForward=False
 TimeDifStr='10/10/2000 '+TimeDifStr
 TimeDif=datetime.datetime.strptime(TimeDifStr, '%d/%m/%Y %H:%M:%S,%f')
-#SubTesting(lines)
-
 for p in places2calc:
         p1Str='10/10/2000 '+lines[p][:12]
         p2Str='10/10/2000 '+lines[p][17:]
@@ -89,19 +79,13 @@ for p in places2calc:
         else:
             time_zero = datetime.datetime.strptime('00:00:00', '%H:%M:%S')
             newP1=(p1 - time_zero + TimeDif).time()
-            #newP1=p1+TimeDif
             newP2=(p2 - time_zero + TimeDif).time()
-            #newP2=p2+TimeDif
             p1Str=str(newP1)[:-3]
             p2Str=str(newP2)[:-3]
-            #print(p1Str+' --> '+p2Str)
-        #newline="{:02d}:{:02d}:{:06.3f}".format(int(p1Str[:1]),int(p1Str[2:4]),float(p1Str[5:]))+' --> '+"{:02d}:{:02d}:{:06.3f}".format(int(p2Str[:1]),int(p2Str[2:4]),float(p2Str[5:]))
         newline=p1Str+' --> '+p2Str        
         i=0
         newline=newline.replace('.',',')
         lines[p]=newline
-        #print(lines[p])
-
 file.close()
 ClearScreen()
 newFile="CorrectedSub.srt"
